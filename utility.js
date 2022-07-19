e37d1c9d63b0f4054678738fae729960d2b21f0c
chaiwat <chaiwatc63@nu.ac.th>
2022-07-19 23:38:03 +0700

    addlab

--- utility.js
+++ utility.js
@@ -4,9 +4,10 @@ const request = require("request-promise");
 const EXTERNAL_API=""; // put url
 const accessToken=""; // put access token
 const student = {
-  name: '', // replace with your full name.
-  age: 19, // put your age.
-  gender: '', // replace with your gender
+  id_student:63361467,
+  name: 'chaiwat', // replace with your full name.
+  age: 20, // put your age.
+  gender: 'Male', // replace with your gender
   department: 'CPE'
 };
 
