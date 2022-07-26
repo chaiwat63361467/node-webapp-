471d3a9e69961b7bac7eb0494f0732167b72145f
chaiwat <chaiwatc63@nu.ac.th>
2022-07-26 22:22:42 +0700

    codelab5

--- utility.js
+++ utility.js
@@ -1,8 +1,8 @@
 //utility.js
 
 const request = require("request-promise");
-const EXTERNAL_API=""; // put url
-const accessToken=""; // put access token
+const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODMxNTM2LCJleHAiOjE2NTg4MzUxMzYsIm5iZiI6MTY1ODgzMTUzNiwianRpIjoiYnU2em1oUHNIQU9SOVVBVyIsInN1YiI6MjAsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.X0ZCEkFWMeBEprD7PC358V_GCN5s0nKzuotOlz5qKX8"; // put access token
 const student = {
   id_student:63361467,
   name: 'chaiwat', // replace with your full name.
@@ -15,14 +15,10 @@ const student = {
 exports.findStudentbyId = function (student_id, cb) {
   //-- call external api 
           request({
-            method: "POST",
-            uri: EXTERNAL_API,
+            method: "GET",
+            uri: EXTERNAL_API+student_id,
             headers: {
-                Authorization: `Bearer ${accessToken}`
-            },
-            formData: {
-                message: `HTTP Request :${data.student_id} `,
-                student_id: student_id
+                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODQ0NzkxLCJleHAiOjE2NTg4NDgzOTEsIm5iZiI6MTY1ODg0NDc5MSwianRpIjoiNlFlNmd2Wk1rd3pZR2dQMyIsInN1YiI6MjAsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.Dl1mtArflZx3xdwNKrEdBDvAUwSOH7N17J9KnIkpzVc`
             }
         }).then((response) => {
             console.log('Sent');
@@ -39,9 +35,4 @@ exports.findStudentbyId = function (student_id, cb) {
                 "Error": err.message
             });
         });
-}
-
-exports.fakeStudentbyInfo = function (student_id, cb) {
-
-  cb(student);
 }
